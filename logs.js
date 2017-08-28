9013f2d0aa074895e6e814d283d12730a625b508
Marty Powell <jpowel25@gmail.com>
2017-08-28 11:52:06 -0400

    add

---
+++ logs.js
@@ -0,0 +1,27 @@
+function Logs() {
+    this.Get = function (options, callback) {
+        var date = options.date || null;
+        var filePath = getFullLogPath(date);
+        var dataArr = [];
+
+        function onLine(line) {
+            dataArr.push(JSON.parse(line));
+        }
+
+        function onClose() {
+            return callback(dataArr);
+        }
+
+        readline.createInterface({
+                input: fs.createReadStream(filePath),
+                terminal: false
+            })
+            .on('line', onLine)
+            .on('close', onClose);
+    }
+    this.Filter = function () {
+        //TODO: implement
+    }
+}
+
+module.exports = Logs;

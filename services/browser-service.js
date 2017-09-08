426fffdb57400f9bb792ea015de6094fbb324437
Marty Powell <jpowel25@gmail.com>
2017-09-08 12:38:13 -0400

    converted to es6

--- services/browser-service.js
+++ services/browser-service.js
@@ -1,32 +1,36 @@
-var BrowserService = (function() {
-    function getBrowserInfoFromUserAgent(userAgent, shouldReturnVersion) {
-        var browserInfoArr = parseUserAgentString(userAgent);
+/**
+ * Public Methods
+ */
+const getBrowserInfoFromUserAgent = (userAgent, shouldReturnVersion) => {
+    const browserInfoArr = parseUserAgentString(userAgent);
+    const browserInfoExist  = browserInfoArr & browserInfoArr.length > 0;
 
-        if (shouldReturnVersion) {
-            return browserInfoArr.join(' ');
-        }
-        return browserInfoArr[0];
+    if (shouldReturnVersion && browserInfoExist) {
+        return browserInfoArr.join(' ');
     }
+    return browserInfoExist ? browserInfoArr[0] : null;
+};
 
-    function parseUserAgentString(userAgent) {
-        var tem;
-        var M = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-        if (/trident/i.test(M[1])) {
-            tem = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
-            return 'IE ' + (tem[1] || '');
-        }
-        if (M[1] === 'Chrome') {
-            tem = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
-            if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-        }
-        M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-        if ((tem = userAgent.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
-        return M;
+/**
+ * Private Methods
+ */
+const parseUserAgentString = (userAgent) => {
+    let tem;
+    let M = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if (/trident/i.test(M[1])) {
+        tem = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+        return 'IE ' + (tem[1] || '');
     }
+    if (M[1] === 'Chrome') {
+        tem = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
+        if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+    if ((tem = userAgent.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+    return M;
+};
 
-    return {
-        getBrowserInfoFromUserAgent: getBrowserInfoFromUserAgent
-    };
-})();
 
-module.exports = BrowserService;
+module.exports = {
+    getBrowserInfoFromUserAgent: getBrowserInfoFromUserAgent
+};

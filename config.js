5f85b88cfb39e3c14338884928be5ef231d9356b
Marty Powell <jpowel25@gmail.com>
2017-09-21 18:41:25 -0400

    added mongo connection string

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const configuration = {
     version: "1.0.0",
     development: {
-        dbConnectionStr: "mongodb://localhost:27017/lumberjack-dev"
+        dbConnectionStr: "mongodb://heroku_4pws3sg1:oqbj44003a8ne2sf537q8csptd@ds147274.mlab.com:47274/heroku_4pws3sg1"
     },
     production: {
         dbConnectionStr: "mongodb://localhost:27017/lumberjack-dev"

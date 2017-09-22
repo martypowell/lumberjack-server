c8a5e0fc968ec27a69971d19d085d51de76adf32
Marty Powell <jpowel25@gmail.com>
2017-09-21 20:18:26 -0400

    updated config to use env variables

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
+var dbConnectionStr = process.env.MONGODB_URI || "mongodb://localhost:27017/lumberjack-dev";
 const configuration = {
     version: "1.0.0",
     development: {
-        dbConnectionStr: "mongodb://heroku_4pws3sg1:oqbj44003a8ne2sf537q8csptd@ds147274.mlab.com:47274/heroku_4pws3sg1"
+        dbConnectionStr: dbConnectionStr
     },
     production: {
         dbConnectionStr: "mongodb://localhost:27017/lumberjack-dev"

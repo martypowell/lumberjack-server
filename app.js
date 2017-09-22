08dae75332624fc1bec1eb5bb3c13e8d5003dd3d
Marty Powell <jpowel25@gmail.com>
2017-09-21 20:40:07 -0400

    adding user model and associated packages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 const db = require('./data-access').db;
 const routes = require('./routes');
+const secret = require('./config').secret;
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -10,6 +11,15 @@ var port = process.env.PORT || 8080;
 server.connection({
     host: '0.0.0.0',
     port: port
+});
+
+server.register(require('hapi-auth-jwt'), (err) => {
+    // We're giving the strategy both a name
+    // and scheme of 'jwt'
+    server.auth.strategy('jwt', 'jwt', {
+        key: secret,
+        verifyOptions: { algorithms: ['HS256'] }
+    });
 });
 
 // localhost:4000
--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var dbConnectionStr = process.env.MONGODB_URI || "mongodb://localhost:27017/lumberjack-dev";
 const configuration = {
+    secret: "pH+cu_a!R7cakuhajeHezufr3cU",
     version: "1.0.0",
     development: {
         dbConnectionStr: dbConnectionStr

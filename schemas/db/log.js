c6e7d412b73870f13dc7d578603055eabaeeae5b
Marty Powell <jpowel25@gmail.com>
2017-10-17 22:04:25 -0400

    eslint fixes

--- schemas/db/log.js
+++ schemas/db/log.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 
 var logSchema = mongoose.Schema({
-    appName: String,
-    type: String,
-    message: String,
-    dateCreated: Date
+	appName: String,
+	type: String,
+	message: String,
+	dateCreated: Date
 });
 
-logSchema.pre('save', function(next) {
-  this.dateCreated = new Date();
-  next();
+logSchema.pre('save', function save(next) {
+	this.dateCreated = new Date();
+	next();
 });
 
 module.exports = logSchema;
